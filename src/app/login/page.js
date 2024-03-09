8a4f6b4b157e59e9046eb4b5e640187da42dd8b0
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-10 01:32:45 +0600

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,9 @@
 import Modals from '@/components/authModal/Modals'
+import  { useState } from 'react';
 import React from 'react'
 
 export default function page() {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
     const handleClose = () => setOpen(false);
   return (
     <div>
